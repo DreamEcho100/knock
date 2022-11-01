292976ac1f06497d617cb6fba279953149ef214d
DreamEcho100 <maze6572198@gmail.com>
2022-11-02 00:29:40 +0200

    Bug fix

--- components/screens/ProductById/sections/Hero.tsx
+++ components/screens/ProductById/sections/Hero.tsx
@@ -19,7 +19,7 @@ const HeroSection = ({ product }: IProductByIdPageProps) => {
         />
         <div
           className="container-restrictions-2
-			 			relative flex rounded-3xl overflow-hidden flex-col max-w-[600px]
+			 			relative flex rounded-3xl overflow-hidden flex-col max-w-[400px]
 						lg:flex-row lg:max-w-full"
         >
           <Image
@@ -39,17 +39,22 @@ const HeroSection = ({ product }: IProductByIdPageProps) => {
               <span className="text-bg-secondary-1">Shipping</span>
               &nbsp;calculated at checkout.
             </p>
-            <p className="flex">
+            <p className="flex flex-col">
               Pay in 4 interest-free installments of $12.50 with
               <Image
                 src="/images/shoppay.png"
                 alt="shop pay"
                 width={200}
                 height={50}
-                className="object-contain w-28"
+                className="object-contain w-28 aspect-video"
               />
+              {/* <span className='w-28'>
+              </span> */}
             </p>
-            <div className="flex justify-between">
+            <div
+              className="flex justify-center items-center flex-wrap gap-4
+									lg:justify-between"
+            >
               <Button className="capitalize mt-2">add to cart</Button>
               <Image
                 src="/images/payment_cards.png"
