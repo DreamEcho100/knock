b900127c96626f1a2aaf1b46e538bcc546634266
DreamEcho100 <maze6572198@gmail.com>
2022-11-02 11:16:34 +0200

    Minor changes after the feedback

--- components/screens/ProductById/sections/Description.tsx
+++ components/screens/ProductById/sections/Description.tsx
@@ -12,8 +12,8 @@ const DescriptionSection = ({
   return (
     <section className="bg-primary-1 overflow-x-hidden">
       <div
-        className={`${classes.contentContainerElements} ${classes.contentContainerV2} flex flex-col text-center container-restrictions-1 leading-10
-				p-20`}
+        className={`${classes.contentContainerElements} ${classes.contentContainerV2} flex flex-col text-center container-restrictions-1 leading-10 p-4
+				sm:p-20`}
       >
         <MdToHTMLFormatter content={description} />
       </div>
