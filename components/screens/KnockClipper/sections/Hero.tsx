d69191c5c47f7375ad223b948104cad7fbc98ba1
DreamEcho100 <maze6572198@gmail.com>
2022-11-03 13:20:41 +0200

    Minor changes after the feedback

--- components/screens/KnockClipper/sections/Hero.tsx
+++ components/screens/KnockClipper/sections/Hero.tsx
@@ -8,7 +8,7 @@ const HeroSection = () => {
     <section className="bg-primary-1">
       <div
         className="container-restrictions-1 overflow-hidden
-				px-8 pt-24 pb-16 flex items-center justify-center flex-col text-center"
+				px-8 pt-24 pb-24 flex items-center justify-center flex-col text-center"
       >
         <div className="relative flex items-center justify-center max-w-[900px]">
           <Image
