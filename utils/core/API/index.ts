760c75e2bcc61b746c0d4c99b6f4a357e8fdda97
DreamEcho100 <maze6572198@gmail.com>
2022-11-06 02:53:09 +0200

    Bugs fixes

--- utils/core/API/index.ts
+++ utils/core/API/index.ts
@@ -4,7 +4,7 @@ const getAppApiPath = () =>
 	typeof window === 'undefined'
 		? // !!!
 		  process.env.NEXT_PUBLIC_VERCEL_URL
-			? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+			? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}/api`
 			: process.env.NEXT_PUBLIC_BACKEND_ABSOLUTE_PATH
 		: process.env.NEXT_PUBLIC_BACKEND_RELATIVE_PATH;
 
