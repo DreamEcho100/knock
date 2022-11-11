c5fc8f28079a9b06426c547424483f3d4d80dbab
DreamEcho100 <maze6572198@gmail.com>
2022-11-11 22:04:41 +0200

    Minor changes after the feedback

--- components/screens/KnockPlugin/sections/Reviews.tsx
+++ components/screens/KnockPlugin/sections/Reviews.tsx
@@ -3,199 +3,234 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 
 const reviews = [
 	{
-		image: { src: '/images/artists/Baby Keem.png', alt: 'Baby Keem' },
+		image: { src: '/images/testimonials/Dara JT..webp', alt: 'Dara JT.' },
 		review:
 			"You're a Trendsetter,\nTrailblazer, Innovator, Mogul, and Inspiration to everyone\naround you. Your Drums Will Always stand the test of Time\nfor many generations of creatives. More Blessings on Blessings.",
 		reviewBy: 'Dara JT.'
 	},
 	{
-		image: { src: '/images/artists/Baby Keem.png', alt: 'Baby Keem' },
+		image: { src: '/images/testimonials/AN TY.jpg', alt: 'AN TY' },
 		review:
 			"LEGEND. Not one of my songs I've released\nin the past 5 years has been made without at least one\nelement of any of the DTK.",
 		reviewBy: 'AN TY'
 	},
 	{
-		image: { src: '/images/artists/Baby Keem.png', alt: 'Baby Keem' },
+		image: { src: '/images/testimonials/icon1.png', alt: '' },
 		review:
 			"Dude, your sound packs to many were like a real inside look into what the words biggest\nproducers are working with and why their music sounded as it did. When you made it\naccessible for everyone to have you've literally bridge a HUGE gap between pros with\nexpensive equipment and kids with laptops to a point where everyone is able to focus\nmore on creativity and not stress out about the quality of your beats. We are forever\nthankful for that",
 		reviewBy: 'From The Crib'
 	},
 	{
-		image: { src: '/images/artists/Baby Keem.png', alt: 'Baby Keem' },
+		image: { src: '/images/testimonials/LLusion.jpg', alt: 'LLusion' },
 		review:
 			'DTK truly got me through some tough times. Still does to this day. About to download X\ntonight and make some crazy shit. Praise DECAP',
 		reviewBy: 'LLusion'
 	},
 	{
-		image: { src: '/images/artists/Baby Keem.png', alt: 'Baby Keem' },
+		image: { src: '/images/testimonials/Toasty_MM.jpeg', alt: 'Toasty_MM' },
 		review:
 			"Helped me elevate my own sound and gave me the confidence to actually send out my\nmusic. Without Decap I would have never to make the music I've made with artists\ntoday. Thank you brother.",
 		reviewBy: 'Toasty_MM'
 	},
 	{
-		image: { src: '/images/artists/Baby Keem.png', alt: 'Baby Keem' },
+		image: {
+			src: '/images/testimonials/Pri$EGotBeats.jpg',
+			alt: 'Pri$EGotBeats'
+		},
 		review:
 			"He (Decap) really does have some of the best sounds in the game, if not the best.\nmean you really don't have to do anything to them, when mixing your beat. Kicks are\npowerful, Snare crack, and the rest is top tier quality. Thanks for all your hard work an\neffort.",
 		reviewBy: 'Pri$EGotBeats'
 	},
 	{
-		image: { src: '/images/artists/Baby Keem.png', alt: 'Baby Keem' },
+		image: { src: '/images/testimonials/icon1.png', alt: '' },
 		review:
 			'DECAP kits are the best. You know it, I know it; Everybody knows it!',
 		reviewBy: 'Pro Ave'
 	},
 	{
-		image: { src: '/images/artists/Baby Keem.png', alt: 'Baby Keem' },
+		image: { src: '/images/testimonials/Flume.jpg', alt: 'Flume' },
 		review: 'Thnx for keeping me inspired.',
 		reviewBy: 'Flume'
 	},
 	{
-		image: { src: '/images/artists/Baby Keem.png', alt: 'Baby Keem' },
+		image: { src: '/images/testimonials/Ipkuss.jpg', alt: 'Ipkuss' },
 		review:
 			"Decap is literally helping shape the sound of popular modern production, it's trippy.",
 		reviewBy: 'Ipkuss'
 	},
 	{
-		image: { src: '/images/artists/Baby Keem.png', alt: 'Baby Keem' },
+		image: {
+			src: '/images/testimonials/KAELIN ELLIS.jpg',
+			alt: 'KAELIN ELLIS'
+		},
 		review: 'Thank you Decap for the drums that knock.',
 		reviewBy: 'KAELIN ELLIS'
 	},
 	{
-		image: { src: '/images/artists/Baby Keem.png', alt: 'Baby Keem' },
+		image: { src: '/images/testimonials/THE KOUNT.jpg', alt: 'THE KOUNT' },
 		review: 'Decap shit slaps unreasonably hard',
 		reviewBy: 'THE KOUNT'
 	},
 	{
-		image: { src: '/images/artists/Baby Keem.png', alt: 'Baby Keem' },
+		image: {
+			src: '/images/testimonials/Monte booker.jpg',
+			alt: 'Monte booker'
+		},
 		review:
 			'They gotta add in the amount of hours we all use your kits in this.',
 		reviewBy: 'Monte booker'
 	},
 	{
-		image: { src: '/images/artists/Baby Keem.png', alt: 'Baby Keem' },
+		image: { src: '/images/testimonials/FrankPole.jpg', alt: 'FrankPole' },
 		review: 'DECAP shaped the music industry according to his style Legend.',
 		reviewBy: 'FrankPole'
 	},
 	{
-		image: { src: '/images/artists/Baby Keem.png', alt: 'Baby Keem' },
+		image: {
+			src: '/images/testimonials/shamtrax music.jpg',
+			alt: 'shamtrax music'
+		},
 		review: 'Refreshing sound design EVERY time!',
 		reviewBy: 'shamtrax music'
 	},
 	{
-		image: { src: '/images/artists/Baby Keem.png', alt: 'Baby Keem' },
+		image: { src: '/images/testimonials/Gustavs.jpg', alt: 'Gustavs' },
 		review: 'The best drum kits out there. I got all of them!',
 		reviewBy: 'Gustavs'
 	},
 	{
-		image: { src: '/images/artists/Baby Keem.png', alt: 'Baby Keem' },
+		image: { src: '/images/testimonials/Spizzyspose.jpg', alt: 'Spizzyspose' },
 		review: 'Instant buy',
 		reviewBy: 'Spizzyspose'
 	},
 	{
-		image: { src: '/images/artists/Baby Keem.png', alt: 'Baby Keem' },
+		image: { src: '/images/testimonials/Blvkbeats.jpg', alt: 'Blvkbeats' },
 		review: 'Best drums in the game',
 		reviewBy: 'Blvkbeats'
 	},
 	{
-		image: { src: '/images/artists/Baby Keem.png', alt: 'Baby Keem' },
+		image: {
+			src: '/images/testimonials/Ecklectic official.jpg',
+			alt: 'Ecklectic'
+		},
 		review: 'The best drums in the industry period!',
 		reviewBy: 'Luis Garay'
 	},
 	{
-		image: { src: '/images/artists/Baby Keem.png', alt: 'Baby Keem' },
+		image: {
+			src: '/images/testimonials/Ecklectic official.jpg',
+			alt: 'Ecklectic official'
+		},
 		review: 'Decap with my FAVE kits.',
 		reviewBy: 'Ecklectic official'
 	},
 	{
-		image: { src: '/images/artists/Baby Keem.png', alt: 'Baby Keem' },
+		image: { src: '/images/testimonials/icon1.png', alt: '' },
 		review: 'These drums will probably win in a fight against Mike Tyson.',
 		reviewBy: 'Adiktive'
 	},
 	{
-		image: { src: '/images/artists/Baby Keem.png', alt: 'Baby Keem' },
+		image: { src: '/images/testimonials/icon1.png', alt: '' },
 		review: 'Nothing beats the joy of getting a Decap drumkit.',
 		reviewBy: 'Naveedsperspective'
 	},
 	{
-		image: { src: '/images/artists/Baby Keem.png', alt: 'Baby Keem' },
+		image: {
+			src: '/images/testimonials/Amnesiaxbeats.jpg',
+			alt: 'Amnesiaxbeats'
+		},
 		review: 'U have the dopest drums in the game rn. I use them a lot.',
 		reviewBy: 'Amnesiaxbeats'
 	},
 	{
-		image: { src: '/images/artists/Baby Keem.png', alt: 'Baby Keem' },
+		image: { src: '/images/testimonials/nata beats.jpg', alt: 'nata beats' },
 		review: 'One of the best sound designers in the game!',
 		reviewBy: 'nata beats'
 	},
 	{
-		image: { src: '/images/artists/Baby Keem.png', alt: 'Baby Keem' },
+		image: { src: '/images/testimonials/icon1.png', alt: '' },
 		review: 'The best kits around!',
 		reviewBy: 'Strong Maurice'
 	},
 	{
-		image: { src: '/images/artists/Baby Keem.png', alt: 'Baby Keem' },
+		image: { src: '/images/testimonials/Wearejayem.webp', alt: 'Wearejayem' },
 		review: 'These drums really knock bro!!!!',
 		reviewBy: 'Wearejayem'
 	},
 	{
-		image: { src: '/images/artists/Baby Keem.png', alt: 'Baby Keem' },
+		image: { src: '/images/testimonials/Maadrhino.jpg', alt: 'Maadrhino' },
 		review: "I'm gonna get all of them!",
 		reviewBy: 'Maadrhino'
 	},
 	{
-		image: { src: '/images/artists/Baby Keem.png', alt: 'Baby Keem' },
+		image: { src: '/images/testimonials/vaportwitch.jpg', alt: 'vaportwitch' },
 		review: 'Legendary. Changed the game for a generation of producers.',
 		reviewBy: 'vaportwitch'
 	},
 	{
-		image: { src: '/images/artists/Baby Keem.png', alt: 'Baby Keem' },
+		image: {
+			src: '/images/testimonials/Tradevoorhees.jpg',
+			alt: 'Tradevoorhees'
+		},
 		review:
 			'Best drums in the game. real shit. I use your kicks on everything. even my own live\ndrums.',
 		reviewBy: 'Tradevoorhees'
 	},
 	{
-		image: { src: '/images/artists/Baby Keem.png', alt: 'Baby Keem' },
+		image: { src: '/images/testimonials/goodxj.jpg', alt: 'goodxj' },
 		review: 'Best drums in the game!!! #dtk',
 		reviewBy: 'goodxj'
 	},
 	{
-		image: { src: '/images/artists/Baby Keem.png', alt: 'Baby Keem' },
+		image: { src: '/images/testimonials/Moorekismet.jpg', alt: 'Moorekismet' },
 		review: 'Take my fucking money',
 		reviewBy: 'Moorekismet'
 	},
 	{
-		image: { src: '/images/artists/Baby Keem.png', alt: 'Baby Keem' },
+		image: {
+			src: '/images/testimonials/Donelljonesforever.jpg',
+			alt: 'Donelljonesforever'
+		},
 		review: 'I love your drums fam. Consider it bought…',
 		reviewBy: 'Donelljonesforever'
 	},
 	{
-		image: { src: '/images/artists/Baby Keem.png', alt: 'Baby Keem' },
+		image: { src: '/images/testimonials/icon1.png', alt: '' },
 		review:
 			'Welp, this about to be the drum sounds on every track for the foreseeable future haha',
 		reviewBy: 'Ambedo_bass'
 	},
 	{
-		image: { src: '/images/artists/Baby Keem.png', alt: 'Baby Keem' },
+		image: {
+			src: '/images/testimonials/of the trees.jpg',
+			alt: 'of the trees'
+		},
 		review: 'best kick samples i always come back to are DECAP',
 		reviewBy: 'of the trees'
 	},
 	{
-		image: { src: '/images/artists/Baby Keem.png', alt: 'Baby Keem' },
+		image: { src: '/images/testimonials/icon1.png', alt: '' },
 		review:
 			'Goes without saying but DECAP got the best drum kits out no question about it.',
 		reviewBy: 'WIZE'
 	},
 	{
-		image: { src: '/images/artists/Baby Keem.png', alt: 'Baby Keem' },
+		image: { src: '/images/testimonials/FrankPole2.jpg', alt: 'FrankPole' },
 		review:
 			'DECAP is a pioneer in the music industry. All the recent hip-hop/trap songs released in\nrecent years sound like that because of his sample packs',
 		reviewBy: 'FrankPole'
 	},
 	{
-		image: { src: '/images/artists/Baby Keem.png', alt: 'Baby Keem' },
+		image: {
+			src: '/images/testimonials/DJ Jazzy Jeff.jpg',
+			alt: 'DJ Jazzy Jeff'
+		},
 		review: 'This is a MUST HAVE!!!! WOW!!!',
 		reviewBy: 'DJ Jazzy Jeff'
 	}
 ];
+
+const shortReviews = reviews.filter((review) => review.review.length < 100);
 
 import 'swiper/css';
 import 'swiper/css/navigation';
@@ -218,7 +253,7 @@ const ReviewsSection = () => {
 					}}
 					className='swiper-wrapper-items-center'
 				>
-					{reviews.map((item, index) => (
+					{shortReviews.map((item, index) => (
 						<SwiperSlide
 							key={index}
 							className='h-full px-2 flex items-center justify-center'
