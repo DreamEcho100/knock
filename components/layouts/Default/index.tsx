e2106c87c5e6c9e16ea66a6f5331c545b79993dc
DreamEcho100 <maze6572198@gmail.com>
2022-11-11 18:49:50 +0200

    Minor changes after the feedback

--- components/layouts/Default/index.tsx
+++ components/layouts/Default/index.tsx
@@ -63,8 +63,8 @@ const Footer = () => {
 						<LinksListContainer
 							headerText='useful links'
 							links={[
-								{ href: '/', text: 'Support' },
-								{ href: '/', text: 'terms of service' },
+								// { href: '/', text: 'Support' },
+								// { href: '/', text: 'terms of service' },
 								{ href: '/policies/privacy-policy', text: 'privacy policy' },
 								{ href: '/policies/refund-policy', text: 'refund policy' },
 								{ href: '/policies/shipping-policy', text: 'shipping policy' },
