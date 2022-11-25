138aededda0af2bbe817ed20ef3bd0ae74ce5773
DreamEcho100 <maze6572198@gmail.com>
2022-11-25 23:58:32 +0200

    Some UI changes after the client feedback && Bug fixes

--- components/screens/KnockClipper/sections/Description.tsx
+++ components/screens/KnockClipper/sections/Description.tsx
@@ -3,7 +3,7 @@ import Description from '@components/shared/core/Description';
 const DescriptionSection = () => {
 	return (
 		<section className='bg-primary-2 section-p-v1'>
-			<Description variants={{ 'max-w': 'none' }} className='max-w-[600px]'>
+			<Description variants={{ 'max-w': 'none' }} className='max-w-[610px]'>
 				KNOCK Clipper is a premium quality, user adjustable hard / soft clipper
 				designed by DECAP. It is the CLIP module from his acclaimed plugin,
 				KNOCK. It is inspired by the signature sound of his popular drum kit
