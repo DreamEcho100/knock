cdc8d1691e5575a141aaa97813a504b1021211e6
DreamEcho100 <maze6572198@gmail.com>
2022-11-28 19:53:21 +0200

    UI changes for Customer profile page

--- components/screens/CustomerProfile/index.tsx
+++ components/screens/CustomerProfile/index.tsx
@@ -398,7 +398,7 @@ const CustomerProfileScreen = () => {
 							<span className='text-bg-secondary-1'>{user.data.email}</span> (
 							<Button
 								onClick={() => setIsLoggingOut(true)}
-								disabled={logoutUser.isLoading}
+								disabled={logoutUser.isLoading && logoutUser.isFetching}
 								classesIntent={{ rounded: 'none', p: 'none', theme: 'none' }}
 								className='text-bg-secondary-1 hover:text-violet-600 focus:text-violet-600'
 							>
