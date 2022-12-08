8a6c329459a395de60f7e1b7376e718e089ac9c7
Mahhdi3106 <hamar3106@gmail.com>
2022-12-08 01:53:01 +0100

    adding pluginsBoutique page and api

---
+++ pages/[pluginsBoutique].tsx
@@ -0,0 +1,56 @@
+import type { GetServerSideProps, GetStaticPaths, GetStaticProps, NextPage } from 'next';
+
+import KnockScreen from '@components/screens/knock-pluginboutique';
+import type { IProduct } from 'types';
+import { getAllProducts, getOneProductByHandle } from 'server/controllers/products';
+import { getIdFromGid } from '@utils/core/shopify';
+
+export interface IKnockPluginBoutiqueProps {
+	knockPluginBoutique: IProduct; // ShopifyBuy.Product;
+}
+
+const KnockPluginBoutique: NextPage<IKnockPluginBoutiqueProps> = (props) => {
+	return <KnockScreen {...props} />;
+};
+
+export default KnockPluginBoutique;
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+	const productId = params?.pluginsBoutique;
+	if (typeof productId !== 'string')
+		throw new Error('productId must be a string');
+
+	// !!!
+	// Handle errors
+	const knockPluginBoutique = JSON.parse(
+		JSON.stringify(await getOneProductByHandle(productId))
+	);
+
+	if (!knockPluginBoutique)
+		return {
+			notFound: true
+		};
+
+	return {
+		props: {
+			knockPluginBoutique
+		}
+	};
+};
+
+export const getStaticPaths: GetStaticPaths<{ pluginsBoutique: string }> = async (
+	context
+) => {
+	const paths = await getAllProducts({
+		typesToExclude: ['Sound Editing Software']
+	}).then((products: any) =>
+		products.map((product: any) => ({
+			params: { pluginsBoutique: product.handle }
+		}))
+	);
+
+	return {
+		paths,
+		fallback: 'blocking'
+	};
+};
