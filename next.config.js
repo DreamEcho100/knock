74e27c5f0110e4dfc67dcc9e4fd4c02b85e32bcc
mehdi <hamar3106@gmail.com>
2023-04-24 18:22:36 +0100

    This header helps prevent cross-site scripting (XSS), clickjacking and other code

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
+
+const securityHeaders = [
+	{
+		key: 'X-Frame-Options',
+		value: 'SAMEORIGIN'
+	},
+	{
+		key: 'Content-Security-Policy',
+		value: "frame-ancestors 'self'"
+	}
+];
+
 const nextConfig = (phase, { defaultConfig }) => {
 	return {
 		reactStrictMode: true,
@@ -31,6 +43,54 @@ const nextConfig = (phase, { defaultConfig }) => {
 					source: '/collections',
 					destination: '/',
 					permanent: true
+				}
+			];
+		},
+		headers: async () => {
+			return [
+				{
+					source: '/',
+					headers: securityHeaders
+				},
+				{
+					source: '/knock',
+					headers: securityHeaders
+				},
+				{
+					source: '/knock-clipper',
+					headers: securityHeaders
+				},
+				{
+					source: '/drums-that-knock',
+					headers: securityHeaders
+				},
+				{
+					source: '/faqs',
+					headers: securityHeaders
+				},
+				{
+					source: '/policies/terms-of-service',
+					headers: securityHeaders
+				},
+				{
+					source: '/policies/privacy-policy',
+					headers: securityHeaders
+				},
+				{
+					source: '/policies/refund-policy',
+					headers: securityHeaders
+				},
+				{
+					source: '/policies/shipping-policy',
+					headers: securityHeaders
+				},
+				{
+					source: '/products/:path',
+					headers: securityHeaders
+				},
+				{
+					source: '/boutique/:path',
+					headers: securityHeaders
 				}
 			];
 		}
