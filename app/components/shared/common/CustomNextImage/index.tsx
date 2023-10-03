28262908495999b13439f58bdba451c38a202d4c
DreamEcho100 <maze6572198@gmail.com>
2023-10-03 14:30:27 +0300

    feat: optimizing the load

--- app/components/shared/common/CustomNextImage/index.tsx
+++ app/components/shared/common/CustomNextImage/index.tsx
@@ -1,15 +1,12 @@
 'use client';
 import { cx } from 'class-variance-authority';
-import Image from 'next/image';
+import Image, { ImageProps } from 'next/image';
 import { forwardRef } from 'react';
-
-type ImageProps = Parameters<typeof Image>[0];
 
 export const websiteBasePath = `https://${process.env.NEXT_PUBLIC_APP_DOMAINE}`;
 
 export interface ICustomNextImageProps extends Omit<ImageProps, 'alt'> {
 	className?: string;
-	placeholder?: 'blur' | 'empty';
 	role?: string;
 	alt?: string;
 }
@@ -24,10 +21,10 @@ const CustomNextImage = forwardRef<HTMLImageElement, ICustomNextImageProps>(
 					elem.currentTarget.src = '/svgs/bbblurry.svg';
 					elem.currentTarget.classList.add('no-content');
 				}}
+				placeholder="blur"
 				{...props}
 				className={cx('no-content', props.className)}
-				placeholder={props.placeholder ?? 'empty'}
-				alt={props.alt ? props.alt : ''}
+				alt={props.alt ?? ''}
 			/>
 		);
 	},
