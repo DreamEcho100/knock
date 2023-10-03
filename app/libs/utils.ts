4445a15d4a05d8a5ff2358b0f5b0e80962a6e89a
DreamEcho100 <maze6572198@gmail.com>
2023-10-04 00:27:30 +0300

    feat: optimizing the load

--- app/libs/utils.ts
+++ app/libs/utils.ts
@@ -2,23 +2,35 @@
  *
  * **Warning:** Doesn't work with circler references
  */
-export function getClassInstanceValues<TValue>(obj: TValue) {
-	const objCopy: Record<PropertyKey, unknown> = {};
-
-	let key: keyof TValue;
-	for (key in obj) {
-		if (typeof obj[key] === 'function') continue;
-		objCopy[key] =
-			obj[key] && typeof obj[key] === 'object'
-				? Array.isArray(obj[key])
-					? (obj[key] as unknown[]).map((item) =>
-							obj[key] && typeof obj[key] === 'object'
-								? getClassInstanceValues(item as Record<PropertyKey, unknown>)
-								: item,
-					  )
-					: getClassInstanceValues(obj[key] as Record<PropertyKey, unknown>)
-				: obj[key];
+export function getClassInstanceValues<TValue>(item: TValue): TValue {
+	if (Array.isArray(item)) {
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-return
+		return (item as unknown[]).map((subItem) =>
+			// eslint-disable-next-line @typescript-eslint/no-unsafe-return
+			subItem && typeof subItem === 'object'
+				? getClassInstanceValues(subItem)
+				: subItem,
+		) as TValue;
 	}
 
-	return objCopy as TValue;
+	const itemCopy: Record<PropertyKey, unknown> = {};
+
+	let key: keyof TValue;
+	for (key in item) {
+		if (typeof item[key] === 'function') continue;
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+		itemCopy[key] =
+			item[key] && typeof item[key] === 'object'
+				? Array.isArray(item[key])
+					? (item[key] as unknown[]).map((subItem) =>
+							// eslint-disable-next-line @typescript-eslint/no-unsafe-return
+							subItem && typeof subItem === 'object'
+								? getClassInstanceValues(subItem)
+								: subItem,
+					  )
+					: getClassInstanceValues(item[key] as Record<PropertyKey, unknown>)
+				: item[key];
+	}
+
+	return itemCopy as TValue;
 }
