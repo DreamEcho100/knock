f084df4622c71c6a0e09ca4be21a9d1e48d5f33a
DreamEcho100 <maze6572198@gmail.com>
2023-10-04 17:59:08 +0300

    feat: optimizing the load

--- app/components/DefaultLayout/components/Actions/index.tsx
+++ app/components/DefaultLayout/components/Actions/index.tsx
@@ -1,21 +1,45 @@
 'use client';
+import { useQueries } from '@tanstack/react-query';
 import dynamic from 'next/dynamic';
-import { useEffect, useState } from 'react';
+import { getBanner, getPopup } from '~/utils/core/API';
+import {
+	getGetAccessTokenFromCookie,
+	useGetUserData,
+} from '~/utils/core/hooks';
 
-const Actions_ = dynamic(() => import('./Actions_'), { ssr: false });
+const MarketingPopupDynamic = dynamic(
+	() => import('~/app/components/shared/common/MarketingPopup/MarketingPopup'),
+	{ ssr: false },
+);
 
 export default function Actions() {
-	const [isActive, setIsActive] = useState(false);
+	const accessToken = getGetAccessTokenFromCookie();
 
-	useEffect(() => {
-		const timeoutId = setTimeout(() => setIsActive(true), 5000);
+	const [popup] = useQueries({
+		queries: [
+			{
+				queryKey: ['get-popup'],
+				queryFn: getPopup,
+				refetchOnWindowFocus: true,
+			},
+			{
+				queryKey: ['banner-data'],
+				queryFn: getBanner,
+				refetchOnWindowFocus: true,
+			},
+		],
+	});
 
-		return () => {
-			clearTimeout(timeoutId);
-		};
-	}, []);
+	useGetUserData({
+		enabled: !!accessToken,
+		accessToken: accessToken,
+	});
 
-	if (!isActive) return <></>;
-
-	return <Actions_ />;
+	return (
+		<>
+			{popup.data && !popup.data.disable && (
+				<MarketingPopupDynamic popup={popup.data} />
+			)}
+		</>
+	);
 }
