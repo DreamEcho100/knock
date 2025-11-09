88a7f1058c4a2a8f58d9f5e2f0f9b6319a9ed6ef
Mazen Mohamed Shaban <mazen.mohamed@viavisolutions.com>
2025-11-09 04:48:13 +0200

    fix: improve key assignment in Logo component for better React performance

--- app/_components/shared/core/Logo/index.tsx
+++ app/_components/shared/core/Logo/index.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import KnockTrademark from '../KnockTrademark';
+import { Fragment } from 'react';
 
 interface IProps extends Partial<Parameters<typeof Link>[0]> {
 	whatKnocks: string;
@@ -12,13 +13,13 @@ const Logo = ({ whatKnocks, ...props }: IProps) => {
 			href="/"
 			className="font-black whitespace-nowrap flex flex-wrap font-decapv16"
 		>
-			{whatKnocks.split(" ").map((word, i, arr) => {
-					if (i === arr.length - 1) {
-						return <KnockTrademark tradeMarkPrefix={word} />
-					}
+			{whatKnocks.split(' ').map((word, i, arr) => {
+				if (i === arr.length - 1) {
+					return <KnockTrademark tradeMarkPrefix={word} key={word} />;
+				}
 
-					return <>{word}&nbsp;</>
-				})}
+				return <Fragment key={word}>{word}&nbsp;</Fragment>;
+			})}
 		</Link>
 	);
 };
